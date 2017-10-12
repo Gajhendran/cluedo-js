13d388cedc2bac4351ca779aa4c0f6afe16e4212
Tom Kusonsuphanimit <16kusonsuphanimittom@colchsfc.ac.uk>
2017-10-12 13:44:27 +0000

    get colours from array

--- client/play/client.1.js
+++ client/play/client.1.js
@@ -5,7 +5,6 @@ var board;
 var next;
 var cursor_x_pos;
 var cursor_y_pos;
-var value;
 
 // Character object
 var character = {
@@ -28,28 +27,53 @@ function setup() {
   for (var i = 0; i < 20; i++) {
     board[i] = new Array(19);
   }
-  
 }
 
 function draw() {
-  draw_grid(board, 20, 20, character);
+  board[0][0] = 0;
+  populate_board(board)
+  draw_grid(board, 20, 20);
 }
 
+// Flush the board 
+//function flush_board(board) {
+//  for (var i = 0; i < 20; i++) {
+//    for (var j = 0; j < 20; i++) {
+//      board[i][j] = 0;
+//    }
+//  }
+//  window.alert("ping")
+//}
+
+// Populate board
+function populate_board(board, character) {
+  board[character.x_position][character.y_position] = character.c
+}
+
+// Draw the grid
 function draw_grid(grid_array, grid_rows, grid_columns) {
   for (var i = 0; i < grid_columns; i++) {
     for (var j = 0; j < grid_rows; j++) {
-      if ((grid_array[i][j] <= 255)) fill(grid_array[i][j]);
-      else fill(255);
+      if ((grid_array[i][j] == 0)) fill(200);
+      else fill(grid_array[i][j);
       stroke(0);
       rect(i * w, j * w, w - 1, w - 1);
     }
   }
+  window.alert("ping")
 }
 
+// What to do when clicked
 function mousePressed() {
   cursor_x_pos = mouseX;
   cursor_y_pos = mouseY;
-  board_x_sel = Math.floor(cursor_x_pos / 480 * 20);
-  board_y_sel = Math.floor(cursor_y_pos / 480 * 20);
-  board[board_x_sel][board_y_sel] = 0;
+  x = Math.floor(cursor_x_pos / 480 * 20);
+  y = Math.floor(cursor_y_pos / 480 * 20);
+  move_character(character, x, y);
+}
+
+// What to do when input to move character
+function move_character(character, x, y) {
+  character.x_position = x
+  character.y_position = y
 }
