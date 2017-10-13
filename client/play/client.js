3e9500ec2a6801c2ed97728ff8960acde9a31d08
Tom Kusonsuphanimit <16kusonsuphanimittom@colchsfc.ac.uk>
2017-10-13 10:25:16 +0000

    added cursor boundry checks, moving peices on the grid, and added character object with colour property

--- client/play/client.js
+++ client/play/client.js
@@ -3,14 +3,12 @@ var columns;
 var rows;
 var board;
 var next;
-var cursor_x_pos;
-var cursor_y_pos;
 var value;
 
 // Character object
 var character = {
-  x_position: 0,
-  y_position: 0,
+  x: 0,
+  y: 0,
   c: 100,
 }
 
@@ -21,20 +19,17 @@ function setup() {
   cursor_y_sel = 0;
   columns = floor(width / w);
   rows = floor(height / w);
-  
   // Create game board array
   // 19x19 for game board, 19x20 for player hold
   board = new Array(20);
   for (var i = 0; i < 20; i++) {
     board[i] = new Array(19);
   }
-  
-  flush_board(board, 200);
-  
+  flush_board(board, 255);
 }
 
 function draw() {
-  draw_grid(board, 20, 20, character);
+  draw_grid(board, 20, 20);
 }
 
 function draw_grid(grid_array, grid_rows, grid_columns) {
@@ -48,17 +43,38 @@ function draw_grid(grid_array, grid_rows, grid_columns) {
 }
 
 function mousePressed() {
-  cursor_x_pos = mouseX;
-  cursor_y_pos = mouseY;
-  board_x_sel = Math.floor(cursor_x_pos / 480 * 20);
-  board_y_sel = Math.floor(cursor_y_pos / 480 * 20);
-  board[board_x_sel][board_y_sel] = 0;
+  // check cursor inside board
+  if (cursor_inside(0, 0, 480, 480)) {
+    board_x = Math.floor(mouseX / 480 * 20);
+    board_y = Math.floor(mouseY / 480 * 20);
+    move_character(character, board, board_x, board_y);
+  }
 }
 
+// Replace values in board with value 
 function flush_board(board, value) {
   for (var i = 0; i < 20; i++) {
     for (var j = 0; j < 20; j++) {
       board[i][j] = value;
     }
+  }
+}
+
+
+function move_character(character, board, x, y) {
+  // swap around values
+  board[character.x][character.y] = 255;
+  board[x][y] = character.c;
+  // set new position
+  character.x = x
+  character.y = y
+}
+
+// Returns if cursor inside rect defined by (x1, y1):(x2, y2)
+function cursor_inside(x1, y1, x2, y2) {
+  if (mouseX >= x1 && mouseY >= y1 && mouseX <= x2 && mouseY <= y2) {
+    return true;
+  } else {
+    return false;
   }
 }
